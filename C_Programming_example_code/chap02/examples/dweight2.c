5e9a47994370df752aa0daa522b95549dbc16dbf
Chowański Sławomir <chowanski@wp.pl>
2023-02-27 18:15:36 +0100

    Literal corect in strings in file dweight2.c

--- C_Programming_example_code/chap02/examples/dweight2.c
+++ C_Programming_example_code/chap02/examples/dweight2.c
@@ -1,4 +1,4 @@
-/* Obliczanie gabarytu przesy³ki o wymiarach wprowadzanych przez u¿ytkownika */
+/* Obliczanie gabarytu przesyÅ‚ki o wymiarach wprowadzanych przez uÅ¼ytkownika */
 
 #include <stdio.h>
 
@@ -6,17 +6,17 @@ int main(void)
 {
   int height, length, width, volume, weight;
 
-  printf("Podaj wysokoœæ przesy³ki: ");
+  printf("Podaj wysokoÅ›Ä‡ przesyÅ‚ki: ");
   scanf("%d", &height);
-  printf("Podaj d³ugoœæ przesy³ki: ");
+  printf("Podaj dÅ‚ugoÅ›Ä‡ przesyÅ‚ki: ");
   scanf("%d", &length);
-  printf("Podaj szerokoœæ przesy³ki: ");
+  printf("Podaj szerokoÅ›Ä‡ przesyÅ‚ki: ");
   scanf("%d", &width);
 
   volume = height * length * width;
   weight = (volume + 165) / 166;
 
-  printf("Objêtoœæ (cale szeœcienne): %d\n", volume);
+  printf("ObjÄ™toÅ›Ä‡ (cale szeÅ›cienne): %d\n", volume);
   printf("Gabaryt (funty rozliczeniowe): %d\n", weight);
 
   return 0;
