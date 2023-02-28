e6a39d65411fa8176cd97348bfdb52e5a524fb13
Chowański Sławomir <chowanski@wp.pl>
2023-02-28 16:31:36 +0100

    Add new files in chapter2

---
+++ C_Programming_example_code/chap02/kola.c
@@ -0,0 +1,34 @@
+/*
+ * kola.c
+ * Copyright 2023 Sławomir Chowański
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#include <stdio.h>
+
+#define PI 3.14
+#define FACTOR 4.0f / 3.0f
+
+int main() {
+
+    int promien = 10;
+    float objetosc = FACTOR * PI * promien * promien *promien;
+
+    printf( "Objętość kóli o promieniu %d cm wynosi %.2f cm3.\n", promien, objetosc );
+
+    return 0;
+}
