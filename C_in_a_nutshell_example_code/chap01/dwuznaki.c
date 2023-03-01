4cadc63d3edd1b82e6b122a2bb985a83fcc319e6
Chowański Sławomir <chowanski@wp.pl>
2023-03-01 20:17:45 +0100

    Adding GPL Licence to my file from chaper 02

--- C_in_a_nutshell_example_code/chap01/dwuznaki.c
+++ C_in_a_nutshell_example_code/chap01/dwuznaki.c
@@ -1,6 +1,22 @@
-// Przykład zastępowania niektórych znaków które mogą nie występować
-// na klawiaturze innymi znakiami (dwóznakami i trójznakami).
-// Sławomir Chowański 2023-02-26
+/*
+ * dwuznaki.c
+ * Copyright 2023 Sławomir Chowański
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
 
 // Znak # można zastąpić dwuznakiem %:
 
